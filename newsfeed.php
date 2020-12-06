603082f9c72ac58c0f1dafd24c71ad35ac13e7c8
ariflateefshah <arif@ideas2it.com>
2020-12-06 22:12:33 +0530

    author detail changes

--- newsfeed.php
+++ newsfeed.php
@@ -1,10 +1,10 @@
 <?php
    /*
    Plugin Name: Newsfeed
-   Plugin URI: https://www.linkedin.com/in/ariflateefshah/
+   Plugin URI: https://github.com/ArifLateefShah/Wordpress-Newsfeed-plugin/
     Version: 1.0
    Author:Arif Shah
-   Author URI: https://www.linkedin.com/in/ariflateefshah/
+   Author URI: https://github.com/ArifLateefShah
    Description: News feed plugin fetch news from https://newsapi.org/.It provides shortcode which can be used inisde posts and pages.
 
    License: GPL2
